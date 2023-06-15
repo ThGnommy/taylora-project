61b8485b00709c306e066a6523e2b720468075aa
ThGnommy <th.brandoli@gmail.com>
2023-06-15 20:07:59 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <h1>Project Builder</h1>
+      <nav>
+        <ul>
+          <li>MODELS</li>
+          <li>COLORS</li>
+          <li>ACCESSORIES</li>
+          <li>SUMMARY</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
